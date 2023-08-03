324484d83308ba746ea83572ba163856337093f5
ProudDesk <timherdemark86@gmail.com>
2023-08-03 19:02:15 +0200

    added ban with id

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -1,4 +1,4 @@
-import { GuildMember, Message, PermissionsBitField } from "discord.js";
+import { Guild, GuildMember, GuildMemberManager, Message, PermissionsBitField } from "discord.js";
 import { ICommand } from "../../ExtendedClient";
 import { UserScheme } from "../../models/models";
 
@@ -16,7 +16,7 @@ export const command: ICommand = {
     },
 
     async execute(message: Message, args: string[]) {
-        const member = message.mentions.members?.first()
+        const member = message.mentions.members?.first() || args[0];
         if (!member) {
             message.reply('you need to mention a user or id!');
             return;
@@ -24,20 +24,34 @@ export const command: ICommand = {
 
         const reason = args.slice(1).join(' ') || 'no reason provided';
 
-        await banUser(member, reason);
+        await banUser(message.guild!, member, reason);
+
         message.channel.send(`${member} has been banned. Reason: ${reason}`);
     }
 };
 
-async function banUser(member: GuildMember, reason: string) {
-    const userId = member.id;
-    const ban = { reason, date: new Date() };
+async function banUser(guild: Guild, member: GuildMember | string, reason: string) {
+    if (member instanceof GuildMember) {
+        const userId = member.id;
+        const ban = { reason, date: new Date() };
+    
+        await UserScheme.findOneAndUpdate(
+            { userId },
+            { $push: { 'punishments.bans': ban } },
+            { upsert: true, new: true }
+        );
+    
+        member.ban({ reason });
+    } else {
+        const userId = member;
+        const ban = { reason, date: new Date() };
+    
+        await UserScheme.findOneAndUpdate(
+            { userId },
+            { $push: { 'punishments.bans': ban } },
+            { upsert: true, new: true }
+        );
 
-    await UserScheme.findOneAndUpdate(
-        { userId },
-        { $push: { 'punishments.bans': ban } },
-        { upsert: true, new: true }
-    );
-
-    member.ban({ reason });
+        guild.members.ban(userId, { reason });
+    }
 }
